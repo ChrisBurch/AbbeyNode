877a6b3c9845658b4267da7ef6390ed9d482397a
Yusuf Simonson <simonson@gmail.com>
2011-07-30 06:59:44 -0400

    removed test files

--- songs.js
+++ songs.js
@@ -5,16 +5,20 @@ var SHEETS_DIR = __dirname + "/static/sheets";
 var SONGS_DIR = __dirname + "/static/songs";
 var NAME_VALIDATION_REGEX = /^[a-zA-Z0-9 _-]+$/;
 
+//song names associated with existing sheets
 var sheets = _.map(fs.readdirSync(SHEETS_DIR), function(file) {
     return file.substring(0, file.length - 5);
 });
 
+//song names associated with existing mp3's
 var songs = _.map(fs.readdirSync(SONGS_DIR), function(file) {
     return file.substring(0, file.length - 4);
 });
 
+//legit songs, which must have both an mp3 and a sheet
 var workingSongs = _.intersection(sheets, songs);
 
+//validates a song name, which can only contain letters, numbers, spaces, underscores and dashes
 function validateName(name) {
     if(name.match(NAME_VALIDATION_REGEX) == null) {
         console.error("Illegal song name:", name);
