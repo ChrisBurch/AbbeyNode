a3082e1d0ccaa49c40035d4353dab9713682c561
Chris Burchhardt <cburchhardt@linkedin.com>
2011-07-30 09:54:47 -0700

    hahahah

--- static/js/app.js
+++ static/js/app.js
@@ -1,173 +1,3 @@
-// /* Main Application File.  Order is important! */
-// 
-// /**
-//  * Checks to see if the timestamp lies within the leeway.
-//  *
-//  * @param timestamp The timestamp of the keypress.
-//  * @return boolean
-//  */
-// function checkRange(timestamp) {
-//   return timestamp >= counter - constants.LEEWAY && timestamp <= counter + constants.LEEWAY;
-// }
-// 
-// /**
-//  * Given a keycode, determine if you successfully hit a note.
-//  * if tick contains a note from the player's instrument
-//  *   if keycode exists within the tick and the keypress is within acceptable limits
-//  *     user succeeds, remove the tick and log a success.
-//  *
-//  * @return boolean
-//  */
-// function checkKeypress(keyCode) {
-//   if(nextTick[instrument].length > 0) {
-//     var tmpKc = $.inArray(keyCode, nextTick[instrument])
-//     if ((tmpKc > -1) && (checkRange(nextTick[0]))) {
-//       nextTick[instrument][tmpKc] = -1;
-//     }
-//   }
-//   console.log(counter + ", " + keyCode);
-// }
-// 
-// /**
-//  * Checks to see if the tick for the user has passed.  If it has, register a note failed event.
-//  * @return boolean
-//  */
-// function checkNextTick() {
-//   if(!nextTick) {
-//     console.log("No next tick found. Quitting.");
-//     stopCounter();
-//     return false;
-//   }
-//   var total = 0;
-//   $.each(nextTick[instrument], function() {
-//     total += this;
-//   });
-//   if(total == nextTick[instrument].length * -1) {
-//     noteSuccess();
-//   } else if(nextTick[0] < (constants.LEEWAY + counter)) {
-//     noteFailure();
-//   }
-// }
-// 
-// /**
-//  * Increases the counter by TICK_INTERVAL milliseconds
-//  */
-// function incrementCounter() {
-//   // Increment counter, then check for the next tick.
-//   checkNextTick();
-//   $('#ticker').text(counter);
-//   counter += constants.TICK_INTERVAL;
-// }
-// 
-// /**
-//  * Removes the tick, sets nextTick.
-//  */
-// function getNextTick() {
-//   console.log(song.length);
-//   if(song.length > 0) {
-//     song.shift();
-//     nextTick = song[0];
-//   } else {
-//     console.log("Nothing left.");
-//     stopCounter();
-//   }
-// }
-// 
-// function ddd() {
-//   console.log("Debug Information");
-//   console.log("Counter: " + counter + "\nCurrent tick: ");
-//   console.log(nextTick);
-//   return false;
-// }
-// 
-// /**
-//  * Stops the song counter.
-//  */
-// function stopCounter() {
-//   clearInterval(counterId);
-//   console.log("Counter stopped.");
-//   $("input").unbind('keydown');
-//   clearInterval(canvasTimerObj);
-// }
-// 
-// /**
-//  * Logs a successfully hit note.
-//  */
-// function noteSuccess() {
-//   getNextTick();
-//   console.log("Note success! Now try and hit " + nextTick[instrument]  + " at " + nextTick[0]);
-// };
-// 
-// /**
-//  * Logs a failed keypress.
-//  */
-// function noteFailure() {
-//   getNextTick();
-//   console.log("Note failure");
-// }
-// 
-// /**
-//  * Start playing!
-//  */
-// function startPlaying() {
-//   console.log("First tick, you should try and hit " + nextTick[instrument] + " at " + nextTick[0]);
-//   // Increment the counter by TICK_INTERVAL.
-//   state = stateMapping.PLAYING;
-//   counterId = setInterval(incrementCounter, constants.TICK_INTERVAL);
-//   canvasTimerObj = createCanvas();
-// }
-// 
-// /* MAIN STUFF GOES HERE LOL */
-// $(document).ready(function() {
-//          var canvasTimerObj;
-//        /* Important variables */
-//        /**
-//         * Data structure of our music sheet.
-//         * [
-//         *   [tick, [keycode, keycode], [keycode], []]
-//         * ]
-//         * Ticks can be anything in ms and are in index 0.
-//         * Instruments are listed below and have keycodes stored.
-//         * If nothing happens then the array for the instrument is empty (but still exists).
-//         */
-//        var instrumentMapping = {
-//          GUITAR:1,
-//          BASS:2,
-//          DRUMS:3,
-//          COWBELL:4,
-//          VOICE:5
-//        };
-// 
-//        var stateMapping = {
-//          PLAYING:1,
-//          STOPPED:-1
-//        };
-// 
-//        /* IMPORTANT!  These counters are kind of a big deal. */
-//        var counter = 0;
-//        var song = [
-//          [4000,[65],[],[],[],[]] // 4000, g(a)
-//        , [6000,[74, 70],[],[],[],[]] // 6000, g(fj)
-//        ];
-// 
-//        var instrument;
-//        var constants = {
-//          TICK_INTERVAL: 100,
-//          LEEWAY: 200
-//        }
-//        var counterId;
-//        var nextTick;
-//        var state;
-//        // Setup:
-//        // 1. Set nextTick.
-//        nextTick = song[0];
-//        // 2. Set user instrument.
-//        instrument = instrumentMapping.GUITAR;
-// 
-//        $("input").keydown(function(event) {
-//          checkKeypress(event.keyCode);
-//        });
-// });
 
 var pressed = [];
 
@@ -211,6 +41,8 @@ function createCanvas() {
 	ctx.fillStyle = BG_COLOR;
 	ctx.fillRect(0, 0, WIDTH, HEIGHT);
 	
+	var hitKeys = {};
+	
 	function step() {
 		var visible = [];
 		var current = [];
@@ -229,7 +61,6 @@ function createCanvas() {
 			if(endNote < startDomain) continue;
 			
 			var myNote = fullNoteData[1 + INSTRUMENT_MAP[instrument]];
-			console.log(fullNoteData, instrument, 1 + INSTRUMENT_MAP[instrument]);
 			if(myNote.length == 0) continue;
 			
 			var computedNoteData = [startNote, endNote, myNote];
@@ -272,21 +103,28 @@ function createCanvas() {
 		ctx.lineTo(MARKER_TIME / DISPLAY_TIME * WIDTH, HEIGHT);
 		ctx.stroke();
 		
-		console.log(pressed, _.uniq(_.flatten(_.map(current, function(c) {
-		    return c[2];
-		}))));
 		
-		var fuckups = _.difference(pressed, _.uniq(_.flatten(_.map(current, function(c) {
-		    return c[2];
-		}))));
-		
-		if(fuckups.length > 0) {
-		    msg("abbey-node/player/oops");
-		}
+        
+        for(var i=0; i<current.length; i++) {
+          var part = current[i];
+          
+          for(var j=0; j<part[2].length; j++) {
+              var note = part[2][j];
+              
+              if($.inArray(note, pressed) > -1) {
+                msg('abbey-node/player/oops');
+              }
+          }
+          pressed =[];
+        }
 	}
 	
 	var startTime = new Date().getTime();
 	canvasTimer = setInterval(step, STEP_TIME);	    
 
 return canvasTimer;
+}
+
+function createStars() {
+    console.log("HOORAY!");
 }
--- static/js/ui.js
+++ static/js/ui.js
@@ -1,31 +1,9 @@
 $().ready(function(){
-	
-	var notes = [
-		{ bgColor: 'red' },
-		{ bgColor: 'orange' },
-		{ bgColor: 'yellow' },
-		{ bgColor: 'green' },
-		{ bgColor: 'blue' }
-	];
-	
 	// var scroller = setInterval(function(){
 	// 	$('#mst_notes').style.left = parseInt( $('#mst_notes').style.left ) + 1 + 'px';
 	// }, 1000);
 	
-	/* ACTUAL FUNCTIONS */
-	
-	function createNote(index){
-		$('#mst_notes').css('background', notes[index].bgColor);
-	}
-	
-	$(window).keydown(function(e){
-		if (e.which == 65) createNote(0);
-			else if (e.which == 83) createNote(1);
-			else if (e.which == 68) createNote(2);
-			else if (e.which == 70) createNote(3);
-			else if (e.which == 71) createNote(4);
-	});
-	
+	/* ACTUAL FUNCTIONS */	
 	$('#landingPage li div').click(function(){
 		$(this).toggleClass('active');
 	})
@@ -42,7 +20,4 @@ $().ready(function(){
 		$(this).toggleClass('opened');
 		$('#mav_dress').fadeToggle();
 	})
-	
-	/* LOAD IN THE NOTES */
-	$('#mst_notes').load('notes.html');
 })
